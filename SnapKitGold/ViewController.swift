45c15a184f19141778184170c6d11d0d6d3dfada
LiuLongChang <1040719208@qq.com>
2016-01-07 11:12:55 +0800

    Initial Commit

---
+++ SnapKitGold/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapKitGold
+//
+//  Created by langyue on 16/1/7.
+//  Copyright © 2016年 langyue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
